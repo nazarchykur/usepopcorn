0aeecd03b0ed4ef1b1904d61b44cdc4d694bf9f3
nazarchykur <nazarchykur@gmail.com>
2023-09-15 14:58:02 +0300

    added an example of PropTypes

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating maxRating={"5"} />
     <StarRating maxRating={10} color="blue" size={36} />
     <StarRating
       messages={["Terrible", "Bad", "Okay", "Good", "Great"]}
