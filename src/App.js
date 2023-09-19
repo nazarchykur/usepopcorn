bf7b35020cb70f8f2bb29981cf6ed78245123a36
nazarchykur <nazarchykur@gmail.com>
2023-09-19 13:08:05 +0300

    added an example of Adding a New Effect Changing Page Title

--- src/App.js
+++ src/App.js
@@ -349,6 +349,16 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     getMovieDetails();
   }, [selectedId]);
 
+  useEffect(() => {
+    if (!title) return;
+    document.title = `Movie | ${title}`;
+
+    return function () {
+      document.title = "usePopcorn";
+      // console.log(`Clean up effect for movie ${title}`);
+    };
+  }, [title]);
+
   return (
     <div className="details">
       {isLoading ? (
